c8a2b0db7c1a0f8105609cfd92dcebbf04b8a05b
이태형 <spdhsrnvl123@naver.com>
2022-08-12 23:20:23 +0900

    🎉line chart 2/3 구현 완료

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+import Price from "./Price";
 
 interface ChartProps {
   coinId: string;
 }
 
 interface ChartCoin {
+  time_open : number;
+  time_close : number;
+  open : string;
   close: string;
   high: string;
   low: string;
   market_cap: number;
-  time_open: number;
   volume: string;
 }
 
@@ -19,9 +23,65 @@ const Chart = ({ coinId }: ChartProps) => {
     fetchCoinHistory(coinId)
   );
   return (
-    <>
-      
-    </>
+    <div>
+      {isLoading ? "Loading chart..." : (
+      <ApexChart 
+      type="line"
+      //series에는 우리가 보내고 싶은 모든 data가 들어 있다. 
+      series={[
+        {
+          name : "sales",
+          data : data?.map((price) => Number(price.close)) as number[]
+        }
+      ]}
+      options={{
+        theme : {
+          mode : "dark"
+        },
+        chart:{
+          height: 500,
+          width:500,
+          toolbar:{
+            show:false
+          },
+          background: "transparent"
+        },
+        grid:{
+          show:false
+        },
+        stroke : {
+          curve:"smooth",
+          width:4
+        },
+        yaxis:{
+          show:false
+        },
+        xaxis:{
+          axisBorder : { show:false },
+          labels:{
+            show:false
+          },
+          axisTicks:{
+            show:false
+          },
+          categories: data?.map(value => value.time_close)
+        },
+        fill:{
+          type : "gradient",
+          gradient:{gradientToColors:["blue"],stops:[0,100]},
+        },
+        colors:["red"],
+        // tooltip은 마우스를 올려두면 보이는 거다.
+        tooltip:{
+          // formatter는 우리한테 값을 넘겨주는 함수다.
+          y:{
+            formatter : (value) => `$ ${value.toFixed(3)}`
+          }
+        }
+      }}
+      />
+      )}
+    </div>
   );
 };
 
