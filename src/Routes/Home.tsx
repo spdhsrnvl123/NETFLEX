8acf60322cd0a0ed350fadaeb28fc9f71263a38e
이태형 <spdhsrnvl123@naver.com>
2022-10-10 12:03:14 +0900

    🎉첫번째 화면 슬라이더 구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,114 @@
-import { useQuery } from "@tanstack/react-query"
-import { getMovies } from "../api";
+import { useQuery } from "@tanstack/react-query";
+import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion"
+import { makeImagePath } from "../utils";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+    background : black;
+`
+
+const Loader = styled.div`
+    height : 20vh;
+    display:flex;
+    justify-content: center;
+    align-items : center;
+`
+
+const Banner = styled.div<{bgPhoto:string}>`
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    padding:60px;
+    background-image : linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${(props) => props.bgPhoto});
+    background-size:cover;
+`
+
+const Title = styled.h2`
+    font-size : 68px;
+    margin-bottom : 20px;
+`
+
+const OverView = styled.p`
+    font-size:30px;
+    width:50%;
+`
+
+const Slider = styled.div`
+    position: relative;
+    top:-100px;
+`
+
+const Row = styled(motion.div)`
+    display:grid;
+    gap:10px;
+    grid-template-columns: repeat(6,1fr);
+    position: absolute;
+    width:100%;
+`
+
+const Box = styled(motion.div)`
+    background-color : white;
+    height: 200px;
+    color :red;
+    font-size:66px;
+`
+
+const rowVariants = {
+    hidden : {
+        x : window.outerWidth + 10,
+    },
+    visible :{
+        x : 0,
+    },
+    exit : {
+        x : -window.outerWidth - 10 ,
+    }
+}
 
 function Home(){
-    const { data, isLoading } =useQuery(["movies","nowPlaying"],getMovies);
-    console.log(data,isLoading);
-
-    return <div style={{backgroundColor : "whitesmoke", height:"200vh"}}>home</div>;
+    const {data,isLoading} = useQuery<IGetMoviesResult>(
+        ["movie","nowPlaying"],
+        getMovies
+    );
+    const [index,setIndex] = useState(0);
+    const increaseIndex = ()=> setIndex((prev)=> prev+1);
+    
+    return(
+        <Wrapper>
+            {isLoading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Banner 
+                        onClick={increaseIndex}
+                        bgPhoto={makeImagePath(data?.results[7].backdrop_path || "")}
+                    >
+                        <Title>{data?.results[7].title}</Title>
+                        <OverView>{data?.results[7].overview}</OverView>
+                    </Banner>
+                    <Slider>
+                        <AnimatePresence>
+                            <Row
+                                variants={rowVariants}
+                                initial = "hidden"
+                                animate = "visible"
+                                exit = "exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {[1,2,3,4,5,6].map((i)=>(
+                                    <Box key={i}>{i}</Box>
+                                ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
+                </>
+            )}
+        </Wrapper>
+    )
 }
 
 export default Home;
