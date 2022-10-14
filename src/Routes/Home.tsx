b9d3783408e1bf3372cb88740b11a8cac91a34e2
이태형 <spdhsrnvl123@naver.com>
2022-10-14 22:36:41 +0900

    ✨슬라이더 구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 const Wrapper = styled.div`
     background : black;
+    padding-bottom: 200px;
 `
 
 const Loader = styled.div`
@@ -43,38 +44,59 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
     display:grid;
-    gap:10px;
+    gap:5px;
     grid-template-columns: repeat(6,1fr);
     position: absolute;
-    width:100%;
+    width: 100%;
 `
 
-const Box = styled(motion.div)`
-    background-color : white;
+const Box = styled(motion.div)<{bgPhoto:string}>`
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    font-size: 66px;
     height: 200px;
-    color :red;
-    font-size:66px;
 `
 
 const rowVariants = {
     hidden : {
-        x : window.outerWidth + 10,
+        x: window.outerWidth + 5,
     },
-    visible :{
-        x : 0,
+    visible : {
+        x : 0
     },
     exit : {
-        x : -window.outerWidth - 10 ,
+        x : -window.outerWidth - 5,
     }
 }
+
+const offset = 6;
 
 function Home(){
     const {data,isLoading} = useQuery<IGetMoviesResult>(
         ["movie","nowPlaying"],
         getMovies
     );
+
+    console.log(data?.results);
+
     const [index,setIndex] = useState(0);
-    const increaseIndex = ()=> setIndex((prev)=> prev+1);
+    const [leaving,setLeaving] = useState(false);
+    console.log(leaving);
+    const increaseIndex = ()=> {
+        if(data){
+            if(leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length -1;
+            const maxIndex = Math.floor(totalMovies / offset) -1;
+            setIndex((prev)=>(prev === maxIndex ? 0 : prev + 1))
+        }
+    }
+
+    const toggleLeaving = () => setLeaving((prev) => !prev)
+
+    console.log(index);
     
     return(
         <Wrapper>
@@ -84,25 +106,30 @@ function Home(){
                 <>
                     <Banner 
                         onClick={increaseIndex}
-                        bgPhoto={makeImagePath(data?.results[7].backdrop_path || "")}
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
                     >
-                        <Title>{data?.results[7].title}</Title>
-                        <OverView>{data?.results[7].overview}</OverView>
+                        <Title>{data?.results[0].title}</Title>
+                        <OverView>{data?.results[0].overview}</OverView>
                     </Banner>
                     <Slider>
-                        <AnimatePresence>
-                            <Row
-                                variants={rowVariants}
-                                initial = "hidden"
-                                animate = "visible"
-                                exit = "exit"
-                                transition={{ type: "tween", duration: 1 }}
-                                key={index}
-                            >
-                                {[1,2,3,4,5,6].map((i)=>(
-                                    <Box key={i}>{i}</Box>
-                                ))}
-                            </Row>
+                        {/* onExitComplete에 함수를 넣으면 exit이 끝났을 때 실행된다. */}
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                        {/* initial={false}를 사용하면 라우터를 이동해도 슬라이드가 그대로 유지된다. */}
+                        <Row 
+                            variants={rowVariants} 
+                            initial = "hidden"
+                            animate="visible"
+                            exit = "exit"
+                            transition={{ type : "tween", duration : 1 }}
+                            key={index}
+                        >
+                            {data?.results.slice(1).slice(offset*index, offset*index+offset).map((movie)=> (
+                                <Box 
+                                    key={movie.id} 
+                                    bgPhoto={makeImagePath(movie.backdrop_path,"w500")}
+                                />
+                            ))}
+                        </Row>
                         </AnimatePresence>
                     </Slider>
                 </>
